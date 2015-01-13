e029e1d0ef6647c1b2f12a0345080ef0f37d8d99
StevenY <steven.yi@xamarin.com>
2015-01-13 11:16:05 -0800

    Revert "MobileCRM Tests"
    This reverts commit 0f025b08c7c5cf4304cd0aceffb5c6757c1afed2.

---
+++ MobileCRM.Test.Android/PlatformHelpers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xamarin.UITest.Android;
+
+namespace MobileCRM.Test
+{
+    public static class PlatformHelpers
+    {
+        public static void Drag(this AndroidApp app, float x1, float y1, float x2, float y2) {
+            app.DragCoordinates (x1, y1, x2, y2);
+        }
+
+        public static void DismissKeyboard(this AndroidApp app) {
+            app.Back ();
+        }
+    }
+}
+
