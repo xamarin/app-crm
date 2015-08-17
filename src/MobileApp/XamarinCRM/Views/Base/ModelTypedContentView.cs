e23457a0f772130802b11ccf1a71a71922f3259b
Joe Sauve <joe.sauve@xamarin.com>
2015-08-17 11:43:23 -0500

    Improved comments on ModelTypedContentPage and ModelTypedContentView.

--- src/MobileApp/XamarinCRM/Views/Base/ModelTypedContentView.cs
+++ src/MobileApp/XamarinCRM/Views/Base/ModelTypedContentView.cs
@@ -3,18 +3,25 @@ using XamarinCRM.ViewModels.Base;
 
 namespace XamarinCRM.Views.Base
 {
+    /// <summary>
+    /// A generically typed ContentView that enforces the type of its BindingContext according to TViewModel.
+    /// </summary>
     public abstract class ModelTypedContentView<TViewModel> : ContentView where TViewModel : BaseViewModel
     {
+        /// <summary>
+        /// Gets the generically typed ViewModel from the underlying BindingContext.
+        /// </summary>
+        /// <value>The generically typed ViewModel.</value>
         protected TViewModel ViewModel
         {
             get { return base.BindingContext as TViewModel; }
         }
 
         /// <summary>
-        /// Sets the binding context.
+        /// Sets the underlying BindingContext as the defined generic type.
         /// </summary>
-        /// <value>The binding context.</value>
-        /// <remarks>Enforces the proper binding context type at compile time.</remarks>
+        /// <value>The generically typed ViewModel.</value>
+        /// <remarks>Enforces a generically typed BindingContext, instead of the underlying loosely object-typed BindingContext.</remarks>
         public new TViewModel BindingContext
         {
             set 
