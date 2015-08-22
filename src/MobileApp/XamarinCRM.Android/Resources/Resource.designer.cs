1511454c542ba696d61f327fb94778b26be2c407
Joe Sauve <joe.sauve@xamarin.com>
2015-08-22 01:51:46 -0500

    Updated windowBackground color for Android 5.0+ to white. Need to figure out best way to do this on pre-5.0.

--- src/MobileApp/XamarinCRM.Android/Resources/Resource.designer.cs
+++ src/MobileApp/XamarinCRM.Android/Resources/Resource.designer.cs
@@ -2327,29 +2327,29 @@ namespace XamarinCRMAndroid
 		public partial class Color
 		{
 			
-			// aapt resource value: 0x7f0b004d
-			public const int abc_background_cache_hint_selector_material_dark = 2131427405;
-			
 			// aapt resource value: 0x7f0b004e
-			public const int abc_background_cache_hint_selector_material_light = 2131427406;
+			public const int abc_background_cache_hint_selector_material_dark = 2131427406;
+			
+			// aapt resource value: 0x7f0b004f
+			public const int abc_background_cache_hint_selector_material_light = 2131427407;
 			
 			// aapt resource value: 0x7f0b0000
 			public const int abc_input_method_navigation_guard = 2131427328;
 			
-			// aapt resource value: 0x7f0b004f
-			public const int abc_primary_text_disable_only_material_dark = 2131427407;
-			
 			// aapt resource value: 0x7f0b0050
-			public const int abc_primary_text_disable_only_material_light = 2131427408;
+			public const int abc_primary_text_disable_only_material_dark = 2131427408;
 			
 			// aapt resource value: 0x7f0b0051
-			public const int abc_primary_text_material_dark = 2131427409;
+			public const int abc_primary_text_disable_only_material_light = 2131427409;
 			
 			// aapt resource value: 0x7f0b0052
-			public const int abc_primary_text_material_light = 2131427410;
+			public const int abc_primary_text_material_dark = 2131427410;
 			
 			// aapt resource value: 0x7f0b0053
-			public const int abc_search_url_text = 2131427411;
+			public const int abc_primary_text_material_light = 2131427411;
+			
+			// aapt resource value: 0x7f0b0054
+			public const int abc_search_url_text = 2131427412;
 			
 			// aapt resource value: 0x7f0b0001
 			public const int abc_search_url_text_normal = 2131427329;
@@ -2360,11 +2360,11 @@ namespace XamarinCRMAndroid
 			// aapt resource value: 0x7f0b0003
 			public const int abc_search_url_text_selected = 2131427331;
 			
-			// aapt resource value: 0x7f0b0054
-			public const int abc_secondary_text_material_dark = 2131427412;
-			
 			// aapt resource value: 0x7f0b0055
-			public const int abc_secondary_text_material_light = 2131427413;
+			public const int abc_secondary_text_material_dark = 2131427413;
+			
+			// aapt resource value: 0x7f0b0056
+			public const int abc_secondary_text_material_light = 2131427414;
 			
 			// aapt resource value: 0x7f0b004c
 			public const int accent = 2131427404;
@@ -2441,11 +2441,11 @@ namespace XamarinCRMAndroid
 			// aapt resource value: 0x7f0b0045
 			public const int common_signin_btn_light_text_pressed = 2131427397;
 			
-			// aapt resource value: 0x7f0b0056
-			public const int common_signin_btn_text_dark = 2131427414;
-			
 			// aapt resource value: 0x7f0b0057
-			public const int common_signin_btn_text_light = 2131427415;
+			public const int common_signin_btn_text_dark = 2131427415;
+			
+			// aapt resource value: 0x7f0b0058
+			public const int common_signin_btn_text_light = 2131427416;
 			
 			// aapt resource value: 0x7f0b0012
 			public const int dim_foreground_disabled_material_dark = 2131427346;
@@ -2546,11 +2546,11 @@ namespace XamarinCRMAndroid
 			// aapt resource value: 0x7f0b0030
 			public const int switch_thumb_disabled_material_light = 2131427376;
 			
-			// aapt resource value: 0x7f0b0058
-			public const int switch_thumb_material_dark = 2131427416;
-			
 			// aapt resource value: 0x7f0b0059
-			public const int switch_thumb_material_light = 2131427417;
+			public const int switch_thumb_material_dark = 2131427417;
+			
+			// aapt resource value: 0x7f0b005a
+			public const int switch_thumb_material_light = 2131427418;
 			
 			// aapt resource value: 0x7f0b0031
 			public const int switch_thumb_normal_material_dark = 2131427377;
@@ -2597,11 +2597,14 @@ namespace XamarinCRMAndroid
 			// aapt resource value: 0x7f0b003f
 			public const int wallet_link_text_light = 2131427391;
 			
-			// aapt resource value: 0x7f0b005a
-			public const int wallet_primary_text_holo_light = 2131427418;
-			
 			// aapt resource value: 0x7f0b005b
-			public const int wallet_secondary_text_holo_dark = 2131427419;
+			public const int wallet_primary_text_holo_light = 2131427419;
+			
+			// aapt resource value: 0x7f0b005c
+			public const int wallet_secondary_text_holo_dark = 2131427420;
+			
+			// aapt resource value: 0x7f0b004d
+			public const int windowBackground = 2131427405;
 			
 			static Color()
 			{
