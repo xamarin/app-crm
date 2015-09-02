826685f2c5be775ba27b9e5d63cae7b7609d4411
Joe Sauve <joe.sauve@xamarin.com>
2015-09-01 21:03:59 -0700

    Made labels a little prettier during load on the customer detail screen. Converters to the rescue.

---
+++ src/MobileApp/XamarinCRM/Converters/CompanyLabelBooleanToColorConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Globalization;
+
+namespace XamarinCRM
+{
+    public class CompanyLabelBooleanToColorConverter : IValueConverter
+    {
+        #region IValueConverter implementation
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value)
+            {
+                return Color.Black;
+            }
+            return Color.White;
+
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
+
---
+++ src/MobileApp/XamarinCRM/Converters/IndustryLabelBooleanToColorConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using XamarinCRM.Statics;
+
+namespace XamarinCRM
+{
+    public class IndustryLabelBooleanToColorConverter : IValueConverter
+    {
+        #region IValueConverter implementation
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if ((bool)value)
+            {
+                return Palette._006;
+            }
+            return Palette._008;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
+
