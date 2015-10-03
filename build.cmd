41c3304b712f5e0a7577094b28039b9cfb28a52a
Joe Sauve <joe.sauve@xamarin.com>
2015-10-03 14:56:11 -0500

    Updated build.cmd for Windows

--- build.cmd
+++ build.cmd
@@ -1,9 +1,9 @@
 @echo off
 cls
-if not exist automation\FAKE\tools\Fake.exe (
-  .nuget\nuget.exe install FAKE -OutputDirectory automation -ExcludeVersion
+if not exist packages\FAKE\tools\Fake.exe (
+  tools\NuGet\NuGet.exe install FAKE -OutputDirectory packages -ExcludeVersion
 )
-if not exist automation\NUnit.Runners\tools\nunit-console.exe (
-  .nuget\nuget.exe install NUnit.Runners -OutputDirectory automation -ExcludeVersion
+if not exist packages\NUnit.Runners\tools\nunit-console.exe (
+  tools\NuGet\NuGet.exe install NUnit.Runners -OutputDirectory packages -ExcludeVersion
 )
-automation\FAKE\tools\FAKE.exe build.fsx %* 2>&1
+packages\FAKE\tools\FAKE.exe build.fsx %* 2>&1
