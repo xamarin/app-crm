e85685c1ca1b46db01858aadfaac5598ddb1ccc9
Joe Sauve <joe.sauve@xamarin.com>
2015-10-07 14:42:36 -0700

    Making a big commit around build automation. Temporarily keeping a custom-compiled version of FAKE in the repo. See this pull request in the FAKE repo: https://github.com/fsharp/FAKE/pull/970

--- automation/fake/build.fsx
+++ automation/fake/build.fsx
@@ -1,103 +1,91 @@
-#r @"packages/FAKE/tools/FakeLib.dll"
+#r @"tools/FAKE/FakeLib.dll"
 #load "build-helpers.fsx"
 open Fake
 open System
 open System.IO
-open System.Linq
+open System.Linq;
 open BuildHelpers
+open Fake.XMLHelper
 open Fake.XamarinHelper
 
 let androidKeystorePassword = getBuildParamOrDefault "android_keystore_password" "not_provided"
 
-let RelativeToRepoRoot path = 
-    ("../../" + path)
+let googleMapsForAndroidv2ApiKey = getBuildParamOrDefault "google_maps_for_android_v2_api_key" "not_provided"
 
-let mobileAppSourcePath = "src/MobileApp/"
+let mobileAppSourcePath = "../../src/MobileApp/"
 
-let mobileAppRelativeSourcePath = RelativeToRepoRoot mobileAppSourcePath
+let solutionFile = (mobileAppSourcePath + "XamarinCRM.sln")
 
-let solutionFile = (mobileAppRelativeSourcePath + "XamarinCRM.sln")
+let nuGetPackageOutputPath = (mobileAppSourcePath + "packages/")
 
-let nuGetPackageOutputPath = (mobileAppRelativeSourcePath + "packages/")
+let iOSProjectPath = (mobileAppSourcePath + "XamarinCRM.iOS/")
 
-let iOSProjectPath = (mobileAppRelativeSourcePath + "XamarinCRM.iOS/")
+let iOSBuildOutputPath = "src/MobileApp/XamarinCRM.iOS/bin/" // This path isn't relative to ~/automation/fake like the rest of the paths. The tool that uses it runs in the repo's root folder.
 
-let iOSBuildOutputPath = "src/MobileApp/XamarinCRM.iOS/bin/iPhone/"
-
-let androidProjectPath = (mobileAppRelativeSourcePath + "XamarinCRM.Android/")
+let androidProjectPath = (mobileAppSourcePath + "XamarinCRM.Android/")
 
 let androidProjectFile = (androidProjectPath + "XamarinCRM.Android.csproj")
 
 let androidBuildOutputPath = (androidProjectPath + "bin/")
 
+// a function that restores all the packages in the solution to a particular directory, instead of the default, which would be relative to the working directory
 let RestorePackagesToHintPath = 
     Exec "tools/NuGet/NuGet.exe" ("restore " + solutionFile + " -PackagesDirectory " + nuGetPackageOutputPath)
+
+// a function that encapsulates the different iOS combinations
+let TemplatediOSBuild configName platform provisioningCategory =
+
+    CleanDir (iOSBuildOutputPath + configName)
+    
+    RestorePackagesToHintPath
+    
+    iOSBuild (fun defaults ->
+        {defaults with
+            ProjectPath = solutionFile
+            Platform = platform
+            Configuration = ("iOS " + configName + " (" + provisioningCategory + ")")
+            Target = "Build"
+            BuildIpa = true
+        })
+    TeamCityHelper.PublishArtifact (iOSBuildOutputPath + platform + "/" + configName + "/*.ipa")
 
 // You may or may not want all of the following targets for your purposes. Modify to your liking.
 
 // This target is mostly for a sanity check, to make sure the app builds with debug settings.
 Target "ios-iphone-debug" (fun () ->
-    RestorePackagesToHintPath
-    
-    iOSBuild (fun defaults ->
-        {defaults with
-            ProjectPath = solutionFile
-            Platform = "iPhone"
-            Configuration = "iOS Debug (Development)"
-            Target = "Build"
-            BuildIpa = true
-        })
-    TeamCityHelper.PublishArtifact (iOSBuildOutputPath + "Debug/*.ipa")
+    TemplatediOSBuild "Debug" "iPhone" "Development"
 )
 
-// This target is a release build, signed for App Store distribution.
+// This target is an iOS release build, signed for App Store distribution.
 Target "ios-iphone-appstore" (fun () ->
-    RestorePackagesToHintPath
-    
-    iOSBuild (fun defaults ->
-        {defaults with
-            ProjectPath = solutionFile
-            Platform = "iPhone"
-            Configuration = "iOS AppStore (Distribution)"
-            Target = "Build"
-            BuildIpa = true
-        })
-    TeamCityHelper.PublishArtifact (iOSBuildOutputPath + "AppStore/*.ipa")
+    TemplatediOSBuild "AppStore" "iPhone" "Distribution"
 )
 
-// This target is a release build, signed for InHouse distribution.
+// This target is an iOS release build, signed for InHouse distribution.
 Target "ios-iphone-inhouse" (fun () ->
-    RestorePackagesToHintPath
-    
-    iOSBuild (fun defaults ->
-        {defaults with
-            ProjectPath = solutionFile
-            Platform = "iPhone"
-            Configuration = "iOS InHouse (Distribution)"
-            Target = "Build"
-            BuildIpa = true
-        })
-    TeamCityHelper.PublishArtifact (iOSBuildOutputPath + "InHouse/*.ipa")
+    TemplatediOSBuild "InHouse" "iPhone" "Distribution"
 )
 
-// This target is a release build, signed for Ad-Hoc distribution.
+// This target is an iOS release build, signed for Ad-Hoc distribution.
 Target "ios-iphone-adhoc" (fun () ->
-    RestorePackagesToHintPath
-    
-    iOSBuild (fun defaults ->
-        {defaults with
-            ProjectPath = solutionFile
-            Platform = "iPhone"
-            Configuration = "iOS Ad-Hoc (Distribution)"
-            Target = "Build"
-            BuildIpa = true
-        })
-    TeamCityHelper.PublishArtifact (iOSBuildOutputPath + "Ad-Hoc/*.ipa")
+    TemplatediOSBuild "Ad-Hoc" "iPhone" "Distribution"
 )
 
+// this target is an Android release build, signed for AdHoc distribution
 Target "android-release" (fun () ->
-    RestorePackagesToHintPath
 
+    RestorePackagesToHintPath
+    
+    // Delete the (androidProjectPath + "Resources/values/api-keys.xml") file, because of what happens right below.
+    DeleteFile (androidProjectPath + "Resources/values/api-keys.xml")
+
+    // Insert Google Maps For Android v2 API Key into api-keys.xml file in Android project
+    // The actual values/api-keys.xml does not exist in source. It gets copied into place by an MSBuild target in the csproj file, from the valuesTemplate/api-keys.xml file.
+    // So, we place the api key value into the valuesTemplate/api-keys.xml file instead, knowing that it will get copied into place.
+
+    XmlPokeInnerText (androidProjectPath + "Resources/valuesTemplate/api-keys.xml") "(/resources/string[@name='GoogleMapsKey'])[1]" googleMapsForAndroidv2ApiKey
+
+    // Build, sign, and zip-align
     AndroidPackage (fun defaults ->
         {defaults with
             ProjectPath = androidProjectFile
@@ -107,7 +95,7 @@ Target "android-release" (fun () ->
     |> AndroidSignAndAlign (fun defaults ->
         {defaults with
             ZipalignPath = "tools/zipalign"
-            KeystorePath = (mobileAppRelativeSourcePath + "XamarinCRMAndroid.keystore")
+            KeystorePath = (mobileAppSourcePath + "XamarinCRMAndroid.keystore")
             KeystorePassword = androidKeystorePassword // TODO: Don't store this in the build script for a real app! This gets passed in at the top of the build script.
             KeystoreAlias = "XamarinCRMAndroid"
         })
--- automation/fake/build.sh
+++ automation/fake/build.sh
@@ -1,11 +1,37 @@
 #!/bin/bash
 
-if [ ! -f packages/FAKE/tools/FAKE.exe ]; then
-  mono tools/NuGet/NuGet.exe install FAKE -OutputDirectory packages -ExcludeVersion
+# For now, we're not pulling FAKE from NuGet. I discovered some missing functionality in 
+# FAKE's XMLHelper. In our build.fsx, we take the google maps api key that gets passed 
+# to this bash script, and we modify an XML file in the Android project to contain that key.
+# See the pull request here: 
+# After that pull request is approved:
+#     1) Remove the ~/automation/fake/tools/FAKE directory from the repo.
+#     2) Change the line at the top of buils.fsx to reference "packages/FAKE/tools/FakeLib.dll" instead of "tools/FAKE/FakeLib.dll".
+#     3) Uncomment the following lines, which will make FAKE start pulling from NuGet again, and (hopefully) my improvements to FAKE's XMLHelper.
+
+#if [ ! -f packages/FAKE/tools/FAKE.exe ]; then
+#  mono tools/NuGet/NuGet.exe install FAKE -OutputDirectory packages -ExcludeVersion
+#fi
+
+if [ $# -eq 0 ]; then
+  echo "You must at least provide a build target name."
+  echo "Example: './build.sh [your buid target name]'"
+  exit 1
+else
+  if [ $# -eq 1 ]; then # Assuming the build target is for iOS, and requires no additional parameters. Using this for an Android build will fail, because we require two more parameters for that. See below.
+    mono tools/FAKE/FAKE.exe build.fsx $1 
+  else # Assuming the build target is for Android. Check to make sure both additional parameters are present.
+    if [ $# -ne 3 ]; then
+      echo "You must provide two additional arguments: 1) your android keystore password and 2) your Google Maps For Android v2 API key."
+	    echo "If you're using arguments in addition to the build target name arguments, it assumed you are attempting an Android build."
+	    echo "This particular Android app build requires both an android keystore password and a Google Maps For Android v2 API key."
+	    echo "Example: './build.sh [your buid target name] [your keystore password] [your Google Maps For Android v2 API key]'."
+	    exit 1
+    else
+      # use for our android build, which requires the keystore password for signing the app, and the Google Maps API key because this app uses Maps.
+      mono tools/FAKE/FAKE.exe build.fsx $1 android_keystore_password=$2 google_maps_for_android_v2_api_key=$3
+	  fi
+  fi
 fi
 
-if [ -z "$2" ]; then
-  mono packages/FAKE/tools/FAKE.exe build.fsx $1 # use for iOS
-else
-	mono packages/FAKE/tools/FAKE.exe build.fsx $1 android_keystore_password=$2 # use for android, passing the keystore password
-fi
+
