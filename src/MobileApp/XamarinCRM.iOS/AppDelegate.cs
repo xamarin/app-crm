7cc670721d6deaf7220c7205e4980aadb413de89
Joe Sauve <joe.sauve@xamarin.com>
2015-11-11 11:57:31 -0800

    Updated license in solution file header default settings, and in all code files.

--- src/MobileApp/XamarinCRM.iOS/AppDelegate.cs
+++ src/MobileApp/XamarinCRM.iOS/AppDelegate.cs
@@ -1,5 +1,6 @@
 ﻿
-//
+// The MIT License (MIT)
+// 
 // Copyright (c) 2015 Xamarin
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy of
--- src/MobileApp/XamarinCRM/Converters/OrderListHeaderViewBackgroudColorConverter.cs
+++ src/MobileApp/XamarinCRM/Converters/OrderListHeaderViewBackgroudColorConverter.cs
@@ -1,4 +1,5 @@
-//
+// The MIT License (MIT)
+// 
 // Copyright (c) 2015 Xamarin
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy of
