fa350dabeff91b11e4b53a41341f73d27e186073
James Montemagno <james.montemagno@gmail.com>
2015-11-11 16:10:08 -0800

    New app icon

--- src/MobileApp/XamarinCRM.iOS/Main.cs
+++ src/MobileApp/XamarinCRM.iOS/Main.cs
@@ -1,22 +1,21 @@
-﻿using UIKit;
+using UIKit;
 using Xamarin;
 
 namespace XamarinCRM.iOS
 {
-    public class Application
-    {
-        // This is the main entry point of the application.
-        static void Main(string[] args)
-        {
-            //#if DEBUG
-            //Insights.Initialize(Insights.DebugModeKey);
-            //#else
-            Insights.Initialize("2b82ddc37582e6c1bece7e5901e8bae3bf7bfb26");
-            //#endif
-
-            // if you want to use a different Application Delegate class from "AppDelegate"
-            // you can specify it here.
-            UIApplication.Main(args, null, "AppDelegate");
-        }
-    }
+	public class Application
+	{
+		// This is the main entry point of the application.
+		static void Main (string[] args)
+		{
+			Xamarin.Insights.Initialize (XamarinInsights.ApiKey);
+			//#if DEBUG
+			//Insights.Initialize(Insights.DebugModeKey);
+			//#else
+			//#endif
+			// if you want to use a different Application Delegate class from "AppDelegate"
+			// you can specify it here.
+			UIApplication.Main (args, null, "AppDelegate");
+		}
+	}
 }
